e44960071ac4c2bd8778cd2febdcf53b669c89bf
Andrey Adamovich <gooddha@ya.ru>
2021-10-03 12:50:06 +0300

    done merge endpoint

--- handlers/mergeImages.js
+++ handlers/mergeImages.js
@@ -1,8 +1,27 @@
-const backrem = require('backrem');
+const fs = require('fs');
+const path = require('path');
+const { uploadPath } = require('../utils/upload');
+const { replaceBackground } = require("backrem");
+
 const mergeImages = (req, res) => {
+  const { front, back, color, threshold } = req.query;
 
+  const rgb = color.split(',').map(Number);
+  const thresholdNum = Number(threshold);
 
+  const frontFile = fs.createReadStream(
+    path.resolve(uploadPath, front + '.jpeg')
+  );
 
+  const backFile = fs.createReadStream(
+    path.resolve(uploadPath, back + '.jpeg')
+  );
+
+  replaceBackground(frontFile, backFile, rgb, thresholdNum).then(
+    (readableStream) => {
+      readableStream.pipe(res);
+    }
+  );
 
 }
 
--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.post('/upload', upload.single('image'), uploadImage);
 app.delete('/image/:id', deleteImage);
 app.get('/image/:id', getSingleImage);
 app.get('/list', getList);
-app.get('/merge?front=:frontId&back=:backId&color=:color&threshold=:threshold', mergeImages);
+app.get('/merge', mergeImages);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
